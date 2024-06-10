e8706267e6a355bca4671c7dc75e96a8d7583338
MrBlueSky2001 <smatisa951@iesmartinezm.es>
2024-06-10 20:08:48 +0200

    pruebaBuena

--- main/serializers.py
+++ main/serializers.py
@@ -1,5 +1,18 @@
-class PostSerializer():
-    pass
+from rest_framework import serializers
+from .models import Post, Valoracion
+from django.contrib.auth.models import User
 
-class ValoracionSerializer():
-   pass
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(many=False, queryset=User.objects.all())
+
+    class Meta:
+        model = Post
+        fields = ['titulo','cuerpo','url','user']
+
+
+class ValoracionSerializer(serializers.HyperlinkedModelSerializer):
+    fecha_registro = serializers.ReadOnlyField()
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    class Meta:
+        model = Valoracion
+        fields = ['rating','fecha_registro','comment','url','post','user']
